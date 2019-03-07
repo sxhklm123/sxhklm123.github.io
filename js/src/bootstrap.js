1a9f842bed03c3b3afee3b33686fcb722dd46c51
sxhklm123 <hackddos@qq.com>
2019-03-07 15:00:27 +0800

    Site updated: 2019-03-07 15:00:07

--- js/src/bootstrap.js
+++ js/src/bootstrap.js
@@ -1,7 +1,3 @@
-$(document).ready(function () {
-  Even.backToTop();
-  Even.mobileNavbar();
-  Even.toc();
-  Even.fancybox();
-  Even.visits();
-});
+// build time:Thu Mar 07 2019 14:59:51 GMT+0800 (中国标准时间)
+$(document).ready(function(){Even.backToTop();Even.mobileNavbar();Even.toc();Even.fancybox();Even.visits()});
+//rebuild by neat 
